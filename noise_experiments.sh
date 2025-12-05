f13f33581c70652adffa9f57aa3a230ef2019fad
Andres <andres.torres.villalba@gmail.com>
2025-12-05 10:26:16 -0500

    include dataset config to job script

--- noise_experiments.sh
+++ noise_experiments.sh
@@ -13,16 +13,19 @@ if [ -z "$1" ]; then
 fi
 
 MODEL_VERSION="$1"
+DATASET="$2"
 #input_noise=(.1 .2 .3 .4 .5 .6 .7 .8)
-#input_noise=(.1 .2 .3 .4)
-input_noise=(.5 .6 .7 .8)
+input_noise=(.1 .2 .3 .4)
+#input_noise=(.5 .6 .7 .8)
 
 devices=(0 1 2 3)
 total_devices=${#devices[@]}
 
 for i in "${!input_noise[@]}"; do
     curr_dev=$((i % total_devices))
-    "$PYTHON_INTERPRETER" auto_encoder_train.py --model_version="$MODEL_VERSION" --input_noise="${input_noise[$i]}" --device="$curr_dev" &
+    "$PYTHON_INTERPRETER" auto_encoder_train.py \
+        --model_version="$MODEL_VERSION" --dataset="$DATASET" \
+        --input_noise="${input_noise[$i]}" --device="$curr_dev" &
 done
 
 wait
